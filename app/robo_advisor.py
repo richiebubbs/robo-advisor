d3e1165eb38fb91efb634f7a02c3f8beeed320cf
Rich Bubbico <rjb2182@stern.nyu.edu>
2019-06-18 23:12:52 -0400

    get latest refresh date

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -8,15 +8,18 @@ import json
 #INPUTS:
 request_url ="https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=demo"
 response = requests.get(request_url)
-print(type(response)) #<class 'requests.models.Response'>
-print(response.status_code) #200
-print(response.text)
+#print(type(response)) #<class 'requests.models.Response'>
+#print(response.status_code) #200
+#print(response.text)
 
 parsed_response = json.loads(response.text)
+last_refreshed =  parsed_response['Meta Data']['3. Last Refreshed']
 
 
+#breakpoint()
 
-quit()
+
+#quit()
 
 
 
@@ -26,7 +29,7 @@ print("-------------------------")
 print("REQUESTING STOCK MARKET DATA...")
 print("REQUEST AT: 2018-02-20 02:00pm")
 print("-------------------------")
-print("LATEST DAY: 2018-02-20")
+print(f"LATEST DAY: {last_refreshed} ")
 print("LATEST CLOSE: $100,000.00")
 print("RECENT HIGH: $101,000.00")
 print("RECENT LOW: $99,000.00")
